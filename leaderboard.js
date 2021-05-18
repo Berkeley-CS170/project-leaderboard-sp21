42fb63cee16a93b543c06a461dde688b18d00d2d
Vaibhav Gattani <vaibg@berkeley.edu>
2021-05-18 12:22:37 -0700

    static

--- leaderboard.js
+++ leaderboard.js
@@ -4,68 +4,86 @@ const numInputsPerSize = [300, 300, 300];
 // const broken_files = ['large-135', 'medium-219', 'large-219', 'large-218', 'large-208', 'large-224', 'medium-225', 'large-235']
 
 let teamSet = new Set();
+let firebaseData = null;
+var dict = {}
 
-async function loadTeams(firebase) {
-  const teamSet = new Set();
-  await firebase.database().ref("leaderboard").orderByChild("leaderboard_name").once("value",
-  function(snapshot) {
-    snapshot.forEach(function (item) {
-      teamSet.add(item.val()["leaderboard_name"]);
-    });
-  });
+async function getFirebaseData() {
+    const response = await fetch('https://www.dropbox.com/s/6bnhgv9qk59z2au/cs170-project-spring-2021-default-rtdb-export.json?dl=1');
+    const data = await response.json();
+    console.log(data['teams']);
+    return data;
 }
 
-async function loadAutocomplete(input, firebase) {
+async function loadTeams() {
+  teamSet = new Set();
+  if (firebaseData == null) {
+      firebaseData = await getFirebaseData();
+  }
+  for (let key in firebaseData['leaderboard']) {
+    const item = firebaseData['leaderboard'][key];
+    teamSet.add(item["leaderboard_name"]);
+  }
+  return teamSet;
+}
+
+async function loadAutocomplete(input) {
   if (!teamSet.size) {
-    teams = await loadTeams(firebase);
+    teams = await loadTeams();
     console.warn("Teams not loaded, check pullLeaderboard functions.");
   }
   new Awesomplete(input, {list: Array.from(teamSet)});
 }
 
+
 function round(x) {
     return Math.round((x + Number.EPSILON) * 10000) / 10000;
 }
 
-async function pullLeaderboard(graphName, firebase) {
+async function pullLeaderboard(graphName) {
+    if (firebaseData == null) {
+        firebaseData = await getFirebaseData();
+    }
+
     const entries = [];
-    await firebase.database().ref("leaderboard").orderByChild("input").equalTo(graphName).once("value", function(snapshot) {
-      snapshot.forEach(function(item) {
-        const name = item.val()["leaderboard_name"];
-        const score = round(item.val()["score"]);
-        entries.push([name, score]);
-        teamSet.add(name);
-      });
-    });
+
+    for (let key in firebaseData['leaderboard']) {
+        const entry = firebaseData['leaderboard'][key];
+        if (entry['input'] == graphName) {
+            entries.push([entry['leaderboard_name'], round(entry['score'])]);
+        }
+    }
     return entries.sort((elem1, elem2) => elem2[1] - elem1[1]);
 }
 
 async function pullFullLeaderboard(firebase) {
+    if (firebaseData == null) {
+        firebaseData = await getFirebaseData();
+    }
+
     const leaderboards = {};
-    await firebase.database().ref("leaderboard").orderByChild("input").once("value", function(snapshot) {
-      snapshot.forEach(function(item) {
-        const name = item.val()["leaderboard_name"];
-        const inputName = item.val()["input"];
-        const score = round(item.val()["score"]);
+
+    for (let key in firebaseData['leaderboard']) {
+        const entry = firebaseData['leaderboard'][key];
+        const name = entry['leaderboard_name'];
+        const score = round(entry['score']);
+        const inputName = entry['input'];
         if (!leaderboards.hasOwnProperty(inputName)) {
             leaderboards[inputName] = [];
         }
         leaderboards[inputName].push([name, score]);
-        teamSet.add(name);
-      });
-    });
+    }
     return leaderboards;
 }
 
-async function computeFullLeaderboard(firebase) {
-    const leaderboards = await pullFullLeaderboard(firebase);
+async function computeFullLeaderboard() {
+    const leaderboards = await pullFullLeaderboard();
     const namesAndRanks = {};
     let totalInputs = 0;
     for (let i = 0; i < sizes.length; i++) {
       const size = sizes[i];
       for (let j = 1; j <= numInputsPerSize[i]; j++) {
         const graphName = `${size}-${j}`;
-        // if (broken_files.includes(graphName)) {continue;}
+        if (broken_files.includes(graphName)) {continue;}
         const leaderboard = leaderboards[graphName].sort((elem1, elem2) => elem2[1] - elem1[1]);
         const ranks = getRanks(leaderboard);
         for (let i = 0; i < leaderboard.length; i++) {
@@ -83,11 +101,30 @@ async function computeFullLeaderboard(firebase) {
       }
     }
     const finalEntries = [];
+    const not_full_outputs = [];
     for (let name in namesAndRanks) {
+      const base_scores = namesAndRanks['Welcome_To_The_Leaderboard'];
       const scores = namesAndRanks[name];
       test = scores;
       if (scores.length == totalInputs) {
         const average = scores.reduce((a, b) => a + b[1], 0) / totalInputs;
+        finalEntries.push([name, round(average)]);
+      } else {
+        not_full_outputs.push(name);
+        var score = 0;
+        for (let i = 0; i < base_scores.length; i++) {
+          var found = false;
+          for (let j = 0; j < scores.length; j++) {
+            if (base_scores[i][0] === scores[j][0]) {
+              score = score + scores[j][1];
+              found = true;
+            }
+          }
+          if (!found) {
+            score = score + base_scores[i][1];
+          }
+        }
+        const average = score / totalInputs;
         finalEntries.push([name, round(average)]);
       }
     }
@@ -107,7 +144,6 @@ function getRanks(sortedEntries) {
           currentRank = i + 1;
           prevValue = entry[1];
         }
-
         ranks.push(currentRank);
     }
     return ranks;
@@ -127,9 +163,11 @@ function formatTable(sortedEntries, header, addRanks) {
   }
   for (let i = 0; i < sortedEntries.length; i++) {
       const row = document.createElement('tr');
+      var currentRank = 0
       entry = sortedEntries[i];
       if (addRanks) {
-        const currentRank = ranks[i];
+        currentRank = ranks[i];
+        dict[entry[0]] = currentRank
         const rank = document.createElement('th');
         rank.innerHTML = currentRank;
         row.appendChild(rank);
@@ -141,6 +179,7 @@ function formatTable(sortedEntries, header, addRanks) {
       }
       table.appendChild(row);
   }
+  // console.log(dict)
   return table;
 }
 
@@ -166,14 +205,14 @@ function createTeamView(teamEntries, teamName) {
   );
 }
 
-async function generateRanksForTeam(teamName, firebase) {
-  fullLeaderboardResults = await computeFullLeaderboard(firebase);
+async function generateRanksForTeam(teamName) {
+  fullLeaderboardResults = await computeFullLeaderboard();
   namesAndRanks = fullLeaderboardResults[0];
   return namesAndRanks.hasOwnProperty(teamName) ? namesAndRanks[teamName] : null;
 }
 
-async function generateTeamView(teamName, firebase) {
-   const entries = await generateRanksForTeam(teamName, firebase);
+async function generateTeamView(teamName) {
+   const entries = await generateRanksForTeam(teamName);
    document.getElementById("table").innerHTML = '';
    if (entries == null) {
      const div = document.createElement('h1');
@@ -185,19 +224,19 @@ async function generateTeamView(teamName, firebase) {
    }
 }
 
-async function generateLeaderboard(graphName, firebase) {
+async function generateLeaderboard(graphName) {
     let entries = null;
     let header = null;
     let title = null;
     if (graphName === null) {
-      fullLeaderboardResults = await computeFullLeaderboard(firebase);
+      fullLeaderboardResults = await computeFullLeaderboard();
       entries = fullLeaderboardResults[1];
       header = ["#", "Team Name", "Average Rank"];
       title = "";
     } else {
-      entries = await pullLeaderboard(graphName, firebase);
+      entries = await pullLeaderboard(graphName);
       if (entries.length > 0) {
-        header = ["#", "Team Name", "Score"];
+        header = ["#", "Team Name", "Happiness"];
         title = `<code>${graphName}.in</code>`;
       } else {
         header = [];
